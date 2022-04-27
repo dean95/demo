d0dc381c2a7adebbfe38dc89dd366c6e7a292dc6
Dean Djermanovic <dean.djermanovic@five.agency>
2022-04-27 16:14:47 +0200

    Final

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,40 +1,94 @@
 package com.example.demo
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.NotificationCompat
 import com.example.demo.ui.theme.DemoTheme
 
 class MainActivity : ComponentActivity() {
+
+    val CHANNEL_ID = "CHANNEL_ID"
+    val CHANNEL_NAME = "CHANNEL_NAME"
+
+    val NOTIFICATION_ID = 0
+
+    val notificationManager by lazy { getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        createNotificationChannel()
+
+        // TODO 3: Create PendingIntent
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        // TODO 2: Create notification
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID).apply {
+            setContentTitle("Title")
+            setContentText("Text")
+            setSmallIcon(R.drawable.ic_android_black_24dp)
+            setContentIntent(pendingIntent)
+            setAutoCancel(true)
+        }.build()
+
         setContent {
             DemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting("Android") {
+                        notificationManager.notify(NOTIFICATION_ID, notification)
+                    }
                 }
             }
+        }
+    }
+
+    // TODO 1: Create notification channel
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DemoTheme {
-        Greeting("Android")
+fun Greeting(
+    name: String,
+    onButtonClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = { onButtonClick() }) {
+            Text(text = "Show Notification")
+        }
     }
 }
